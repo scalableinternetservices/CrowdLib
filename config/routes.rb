6ad043de04a8c4bd9da8fe8c3b9e7d776f316a5a
Pritha <pritha.narayanappa@gmail.com>
2015-10-18 17:49:41 -0700

    Moving maps page to /locatebooks

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
       resources :locations, only: []
     end
   end
+#/locatebooks take user to a map showing books in his closest vicinity
+get 'locatebooks', to:      'locatebooks'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

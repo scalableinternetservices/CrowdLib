2af4374c5af7e45793e9f94c0f1f6bdf494bc601
nimisha-srinivasa <nimishasrinivasa174@gmail.com>
2015-10-26 15:23:39 -0700

    Adding the UI, and book entity

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,13 @@
 class Book < ActiveRecord::Base
 	include ActiveModel::Serializers::JSON
 
+	mount_uploader :image_url, ImageUrlUploader # Tells rails to use this uploader for this model.
 	belongs_to :user
 
 	# There are long book names so using a max of 100 chars 
 	# Eg: Proceedings of the Second International Workshop on Nude Mice 
 	
-	validates  :Title, presence: true, length: { maximum: 100 }
-	validates  :Author, presence: true, length: { maximum: 30}
+	validates  :title, presence: true, length: { maximum: 100 }
+	validates  :author, presence: true, length: { maximum: 30}
 
 end

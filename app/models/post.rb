af87b3cf27902007ea6cc496997992f7c1b1ca20
nimisha-srinivasa <nimishasrinivasa174@gmail.com>
2015-10-26 00:40:33 -0700

    Deleting other files related to "Post" entity

--- app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ActiveRecord::Base
-	include ActiveModel::Serializers::JSON
-
-	belongs_to :community
-	belongs_to :user
-
-	has_many :comments
-end
--- db/schema.rb
+++ db/schema.rb
@@ -34,18 +34,6 @@ ActiveRecord::Schema.define(version: 20151019080619) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.integer  "community_id"
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  add_index "posts", ["community_id"], name: "index_posts_on_community_id"
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "firstname"

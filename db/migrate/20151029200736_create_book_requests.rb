edb184ae5ae52b1ab84fc04a6f17cbefe71fc1f9
hemanth <hemanth.umail.ucsb.edu>
2015-11-05 17:27:32 -0800

    request fix

--- db/migrate/20151029200736_create_book_requests.rb
+++ db/migrate/20151029200736_create_book_requests.rb
@@ -6,12 +6,6 @@ class CreateBookRequests < ActiveRecord::Migration
       t.integer :borrower_id
       t.integer :loan_period
       t.boolean :approved
-<<<<<<< Updated upstream
-=======
-      t.references :lender
-      t.references :borrower
->>>>>>> Stashed changes
-
       t.timestamps null: false
     end
   end

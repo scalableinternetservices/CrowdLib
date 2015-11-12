565d4a1a6d5c1ca06cad262b3fb746ed6980b1e1
hemanth <hemanth.umail.ucsb.edu>
2015-11-12 15:58:25 -0800

    minor fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,12 +4,12 @@ class WelcomeController < ApplicationController
 	before_action :authenticate_user!
 	
 	def index
-    		@books = Book.all #where.not(owner_id: current_user.id )
+    	@books = Book.all #where.not(owner_id: current_user.id )
 		@genres = Set.new(Book.all.pluck :genre)
 		@genres = Set.new([]) if @genres.nil?
 		@books = @books.where(genre: params[:genre]) if params[:genre].present?
-    		@books = @books.where(author: params[:author]) if params[:author].present?
-    		@books = @books.where(title: params[:title]) if params[:title].present?
+    	@books = @books.where(author: params[:author]) if params[:author].present?
+    	@books = @books.where(title: params[:title]) if params[:title].present?
 		@unique_authors = Book.uniq.pluck(:author)
 	end
 end

c5ffb9bf92dcb3a61c134db634074608a5ccad17
Ankita Singh <ankitasingh.2309@gmail.com>
2015-12-04 18:38:11 -0800

    fixing auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  skip_before_filter :verify_authenticity_token
+  #skip_before_filter :verify_authenticity_token
 
   #protect_from_forgery with: :exception
 

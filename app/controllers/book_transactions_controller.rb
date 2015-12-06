72e835c38ad1fb919f8caf4ef77ebc5246ee08a1
nimisha-srinivasa <nimishasrinivasa174@gmail.com>
2015-12-05 23:12:07 -0800

    Displaying a the list of books waiting for current users approval

--- app/controllers/book_transactions_controller.rb
+++ app/controllers/book_transactions_controller.rb
@@ -1,5 +1,6 @@
 require 'json'
 class BookTransactionsController < ApplicationController
+
 	#GET /book_transactions/index
 	def index
 		@user = current_user
@@ -15,15 +16,17 @@ class BookTransactionsController < ApplicationController
 				@books_with_user.push(book_wrapper)
 			end
 		end
-		book_txn_waiting_approval = BookTransaction.where("borrower_id=? AND approved='t' AND borrowed='t'",@user.id).where.not("returned='t'")
+		books_owned_by_user = Book.where(:owner_id => @user.id)
 		@books_waiting_approval = Array.new
-		unless book_txn_waiting_approval.nil?
-			book_txn_waiting_approval.each do |book_txn|
-				book=Book.find(book_txn.book_id)
-				@books_waiting_approval.push(book)
-			end
-		end
-
+		books_owned_by_user.each do |book|
+			book_txn = BookTransaction.where(:book_id => book.id).where.not("approved='t' OR borrowed='t' OR returned='t'")
+	      	unless book_txn.nil?
+	        	book_wrapper=Hash.new
+	        	book_wrapper[:book] = book 
+	        	book_wrapper[:book_txn] = book_txn.first
+	        	@books_waiting_approval.push(book_wrapper)
+	      	end
+	    end
 	end
 
 	def request_book
@@ -47,5 +50,4 @@ class BookTransactionsController < ApplicationController
 	    end
 	    render "book_txn_success"
 	end
-
 end
